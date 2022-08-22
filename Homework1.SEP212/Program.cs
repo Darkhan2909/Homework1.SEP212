751a2a43178af0849879a92c89f926dfeb88290c
lukmankhakim.d <lukmankhakim.d@DPU>
2022-08-22 16:04:10 +0600

    Добавьте файлы проекта.

---
+++ Homework1.SEP212/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1.SEP212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер задачи: ");
+
+            int example = Convert.ToInt32(Console.ReadLine());
+
+            if(example == 1)
+            {
+                FirstExample();
+            }
+            else if(example == 2)
+            {
+                SecondExample();
+            }
+
+           
+        }
+
+        //Задание номер 1
+        private static void FirstExample()
+        {
+            Console.WriteLine("Введите Ваше имя: ");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Введите Вашу фамилию: ");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваше отчество: ");
+            string SecondName = Console.ReadLine();
+
+            Console.WriteLine("Привет, меня зовут " + FirstName + " " + LastName + " " + SecondName + "!");
+        }
+
+        //Задание номер 2
+        private static void SecondExample()
+        {
+            Console.WriteLine("Введите первое число: ");
+            string number1 = Console.ReadLine();
+
+            Console.WriteLine("Введите второе число: ");
+            string number2 = Console.ReadLine();
+
+            Console.WriteLine("Сумма, Ваших чисел равна: ");
+            int result = Convert.ToInt32(number1) + Convert.ToInt32(number2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
